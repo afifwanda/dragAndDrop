c32c498082a6b4015e5594245e3a98a65c2692d9
afif julio <afifjulio28@gmail.com>
2020-10-06 10:35:52 +0700

    creating server

---
+++ server/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const multer = require('multer');
+const port = 8181;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+let storage = multer.diskStorage({
+  destination: (req,file,cb) => {
+    cb(null,'./upload')
+  },
+  filename: (req,file,cb) => {
+    cb(null,file.originalname)
+  }
+})
+
+const upload = multer({storage})
+app.use(cors());
+app.use(express.urlencoded({ extended:true }))
+app.use(express.json())
+
+app.use("/file",express.static(__dirname+'/upload'))
+
+app.post('/upload', upload.single('files'), (req, res) => {
+  if (req.file)
+    res.json({
+      fileUrl: `file/${req.file.filename}`
+    });
+  else 
+    res.status("409").json("No Files to Upload.")
+});
+
+app.get('/file')
+
+app.listen(port,function(){
+    console.log("youre listening to port " + port)
+})
+
+module.exports = app
